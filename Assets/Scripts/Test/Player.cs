96dec9e26e87bf42811e9803fb26ea26a6231fec
DezZigeR <dezziger@mail.ru>
2021-04-21 22:10:04 +0300

    Add project files.

---
+++ Assets/Scripts/Test/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace Asteroids.Test
+{
+    internal class Player : IPlayer
+    {
+        public int Hp { get; }
+        public IInventory Inventory { get; }
+        public IWeapon Weapon { get; }
+        
+        public Player(int hp, IInventory inventory, IWeapon weapon)
+        {
+            Hp = hp;
+            Inventory = inventory;
+            Weapon = weapon;
+        }
+    }
+}
